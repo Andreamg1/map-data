69274fcd337dd37ccbc2f4226d038edeb33b6451
Andreamg1 <145478212+Andreamg1@users.noreply.github.com>
2023-12-04 22:59:08 -0600

    Update db.py

--- db.py
+++ db.py
@@ -1,7 +1,10 @@
-# db.py
 from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
+
+
+# Load .env as environment variables
+load_dotenv()
 
 # Connect to cluster with URI
 client = MongoClient(os.environ.get('URI'))
